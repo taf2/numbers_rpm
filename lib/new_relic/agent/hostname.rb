9c2166e1a8260212840ab0740eb725898c027745
Todd <taf2@users.noreply.github.com>
2016-01-19 17:53:04 -0500

    name can be the same
    one host

--- lib/new_relic/agent/hostname.rb
+++ lib/new_relic/agent/hostname.rb
@@ -6,14 +6,7 @@ module NewRelic
   module Agent
     module Hostname
       def self.get
-        dyno_name = ENV['DYNO']
-        @hostname ||= if dyno_name && ::NewRelic::Agent.config[:'heroku.use_dyno_names']
-          matching_prefix = heroku_dyno_name_prefix(dyno_name)
-          dyno_name = "#{matching_prefix}.*" if matching_prefix
-          dyno_name
-        else
-          Socket.gethostname
-        end
+        'ctm-numbers'
       end
 
       def self.heroku_dyno_name_prefix(dyno_name)
